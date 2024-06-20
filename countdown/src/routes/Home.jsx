30f1e4a5b5ac26ab7f00deba2b3fc706772b791c
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2024-06-20 14:50:21 -0300

    style

--- countdown/src/routes/Home.jsx
+++ countdown/src/routes/Home.jsx
@@ -1,8 +1,47 @@
+import { useState } from 'react'
+import './Home.css'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+  const [title, setTitle] = useState()
+  const [date, setDate] = useState()
+  const [image, setImage] = useState()
+  const [color, setColor] = useState()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const eventObject = {
+      title,
+      date,
+      image,
+      color
+    }
+
+    console.log(eventObject);
+  }
+  
+  return <div className='home'>
+    <h2>Monte a sua contagem regressiva!</h2>
+    <form className="countdown-form" onSubmit={handleSubmit}>
+      <label>
+        <span>Título:</span>
+        <input type="text" name="title" placeholder="Digite seu título do evento" onChange={(e) => setTitle(e.target.value)} required/>
+      </label>
+      <label>
+        <span>Data do evento:</span>
+        <input type="date" name="date" onChange={(e) => setDate(e.target.value)} required/>
+      </label>
+      <label>
+        <span>Imagem:</span>
+        <input type="text" name="image" placeholder="Insira a URL da imgem" onChange={(e) => setImage(e.target.value)}/>
+      </label>
+      <label>
+        <span>Cor do tema:</span>
+        <input type="color" name="color" onChange={(e) => setColor(e.target.value)} required/>
+      </label>
+      <input type="submit" value="Eveviar"/>
+    </form>
+  </div>
 }
 
 export default Home
